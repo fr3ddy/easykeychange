8400fe1dc38d87343f5472defce1645730622f84
Freddy <frey@binaerix.de>
2016-11-25 11:12:07 +0100

    changes

---
+++ src/ExportCommand.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Fr3ddy\Easykeychange;
+
+use Illuminate\Console\Command;
+use Excel;
+use Lang;
+use App;
+
+class ExportCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'easykeychange:export';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Export translations to Excel';
+
+    private $keys;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->keys = array();
+        
+        //Search all trans('') in routes / resources / public / app
+        $routes  = \File::allFiles(base_path().'/routes');
+        $resources = \File::allFiles(base_path().'/resources');
+        $public = \File::allFiles(base_path().'/public');
+        $app = \File::allFiles(base_path().'/app');
+        $paths = array_merge($routes , $resources , $public , $app);
+        
+        $this->info('Started searching in files...');
+        $bar = $this->output->createProgressBar(sizeof($paths));
+        
+        $trans = array();
+        foreach($paths as $path){
+            $file = file_get_contents($path);
+            preg_match("/trans\('.+'\)/" , $file , $keys);
+            $trans = array_merge($trans , $keys);
+            preg_match('/trans\(".+"\)/' , $file , $keys);
+            $trans = array_merge($trans , $keys);
+            $bar->advance();
+        }
+        $this->info("");
+        $this->info("Starting with file creation");
+        foreach($trans as $t){
+            $t2 = str_replace("trans('", "" , $t);
+            $t3 = str_replace("')" , "", $t2);
+            $t4 = str_replace('trans("', "" , $t3);
+            $this->keys[] = str_replace('")' , "", $t4);
+        }
+
+        Excel::create('keys', function($excel) {
+
+            // Set the title
+            $excel->setTitle('Easykeychange Export File');
+
+            // Chain the setters
+            $excel->setCreator('Easykeychange')
+                ->setCompany('Fr3ddyF');
+
+            // Call them separately
+            $excel->setDescription('This is the Easykeychange Export File.');
+
+            $excel->sheet('All Keys', function($sheet) {
+                $data = array();
+                $data[] = array('Old Key' , 'New Key');
+                foreach($this->keys as $key){
+                    $data[] = array($key , $key);
+                }
+
+                $sheet->rows($data);
+
+                // Freeze first row
+                $sheet->freezeFirstRow();
+                // Set auto size for sheet
+                $sheet->setAutoSize(true);
+
+                //Format first row
+                $sheet->cells('A1:B1', function($cells) {
+
+                    $cells->setFontSize(14);
+                    $cells->setFontWeight('bold');
+                
+                });
+
+            });
+
+        })->store('xls', storage_path('easykeychange'));
+
+        $this->info('Exported');
+    }
+}
---
+++ src/ImportCommand.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Fr3ddy\Easykeychange;
+
+use Illuminate\Console\Command;
+use Excel;
+
+class ImportCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'easykeychange:import';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import Excel and overwrite existing data';
+
+    private $paths;
+    private $bar;
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //Search all trans('') in routes / resources / public / app
+        $routes  = \File::allFiles(base_path().'/routes');
+        $resources = \File::allFiles(base_path().'/resources');
+        $public = \File::allFiles(base_path().'/public');
+        $app = \File::allFiles(base_path().'/app');
+        $this->paths = array_merge($routes , $resources , $public , $app);
+
+        Excel::load(storage_path('easykeychange').'/keys.xls' , function($reader){
+            $sheet = $reader->first();
+
+            $this->info('Started replacing in files...');
+            $this->bar = $this->output->createProgressBar(sizeof($this->paths));
+            
+            $sheet->each(function($row){
+                $keys = $row->toArray();
+
+                foreach($this->paths as $path){
+                    $file = file_get_contents($path);
+                    if($file){
+                        $count1 = 0;
+                        $count2 = 0;
+                        $file = str_replace("trans('".$keys['old_key']."')" , "trans('".$keys['new_key']."')" , $file , $count1);
+                        $file = str_replace('trans("'.$keys["old_key"].'")' , 'trans("'.$keys["new_key"].'")' , $file , $count2);
+                        if($count1 + $count2 > 0){
+                            $handle = fopen($path,'w');
+                            fwrite($handle , $file);
+                            fclose($handle);
+                        }
+                    }
+                }
+                $this->bar->advance();
+            });
+        });
+
+        $this->info('Everything replaced!');
+    }
+}
